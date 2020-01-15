04b41f70bacebe7faa761b1708f1dde750cc4d17
jonlym <jonathanalym@gmail.com>
2020-01-14 22:15:58 -0500

    Created animals.py

---
+++ animals.py
@@ -0,0 +1,8 @@
+class Dog:
+    def init(self, name, favorite_color='gray'):
+        self.name == name
+        self.favorite_color = favorite_color
+        
+    def bark(self):
+        print(('My name is {} and my favorite color is {}. Er... I mean... '
+               'bark bark'.format(name, favorite_color)))
